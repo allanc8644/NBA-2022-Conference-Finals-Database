ec2ee212b6043dfd53eeec4c38db06aa613ba6bd
allanc8644 <100250793+allanc8644@users.noreply.github.com>
2023-02-14 01:45:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+  
+   cout << "Welcome to the NBA 2022 Conference Finals Database." << endl;
+   cout << "Please select the Conference you would like to view." << endl;
+   cout << endl;
+   cout << "(1) East" << endl;
+   cout << "(2) West" << endl;
+   cin >> choice;
+  
