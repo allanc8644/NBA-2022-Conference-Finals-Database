5e1d803b6c51ac4e801cbefdfc7131b675cdc700
allanc8644 <100250793+allanc8644@users.noreply.github.com>
2023-02-25 16:48:23 -0600

    Update east.cpp

--- east.cpp
+++ east.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-#include <string>
 
-using namespace std;
+#include "east.h"
 
-int gameNum;
-
-void 
+void east::printGame1() {
+	cout << "					Game 1: FTX Arena, Miami, FL					" << endl;
+	cout << " ______________________________________________________________________________________________________________________" << endl;
+	cout << endl;
+	cout << "					      BOS 107 - MIA 118                          " << endl;
+	cout << endl;
+	cout << "(F) Jayson Tatum: 29 PTS    6 AST    8 REB    10/21 FG (47.6%)" << endl;
+	cout << endl;
+	cout << "(F) Grant Williams: 7 PTS    2 AST    1 REB    2/5 FG (40%)" << endl;
+	cout << endl;
+	cout << "(C) Robert Williams III: 18PTS    1 AST    9 REB    6/8 FG (75%)" << endl;
+	cout << endl;
+	cout << "(G) Jaylen Brown: 24 PTS    3 AST    10 REB    7/17 FG (41.2%)" << endl;
+	cout << endl;
+	cout << "(G) Derrick White: 3 PTS    4 AST    0 REB    1/4 FG (25%)" << endl;
+}
