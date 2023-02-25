651050742f28c5110c9ed6d2228bf63a4bf08206
allanc8644 <100250793+allanc8644@users.noreply.github.com>
2023-02-25 16:50:29 -0600

    Create west.h

---
+++ west.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class west {
+
+public:
+	void printGame1();
+	void printGame2();
+	void printGame3();
+	void printGame4();
+	void printGame5();
+
+};
