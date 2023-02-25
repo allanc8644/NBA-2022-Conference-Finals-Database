3d046869c344e85d8f986ce2eef675e8a56968d3
allanc8644 <100250793+allanc8644@users.noreply.github.com>
2023-02-25 16:49:49 -0600

    Create Player.cpp

---
+++ Player.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "Player.h"
+
+using namespace std;
+
+void Player::setPoints(int points) {
+	int points[] = {34, 45, 35, 64, 34, 32, 43};
+}
+
+void Player::printPTS() {
+
+	cout << points[1];
+
+}
+
