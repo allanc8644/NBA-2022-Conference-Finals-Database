b9e0660ba891c1781a00eed36f105975cb650d03
allanc8644 <100250793+allanc8644@users.noreply.github.com>
2023-03-08 00:02:37 -0600

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,25 +1,30 @@
 #include <iostream>
 
+const int gameNum = 6;
+
 class Player {
 
 private:
-	int *points[7];
-	int *assists[7];
-	int *reb[7];
-	float *fgPercent[7];
-	int *fgA[7];
-	int *fGM[7];
+	int points[6];
+	int assists[7];
+	int reb[7];
+	float fgPercent[7];
+	int fgA[7];
+	int fGM[7];
 
 public:
-	void setPoints(int pts);
+	
+	int printPTS(int c) {
+
+		return points[c];
+	}
 	void setAssists(int asts);
 	void setRebounds(int rebounds);
 	void setPercent(float percentage);
 	void setAttepts(int attempt);
 	void setMade(int made);
 
-	void printPTS();
-
+	Player(int[]);
 	//set gets
 	//look at page 772 in book
 	//when in player stats create object for player
