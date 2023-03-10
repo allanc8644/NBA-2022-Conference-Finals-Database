99943d5209d53f9e42908ca606767864474be410
allanc8644 <100250793+allanc8644@users.noreply.github.com>
2023-03-10 00:06:09 -0600

    Update east.cpp

--- east.cpp
+++ east.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
 #include "east.h"
+#include "Player.h"
+
+using namespace std;
+
+Player JayTatum(29, 27, 10, 31, 22, 30, 26,
+	6, 5, 4, 5, 9, 4, 6,
+	8, 5, 6, 8, 12, 9, 10);
 
 void east::printGame1() {
 	cout << "					Game 1: FTX Arena, Miami, FL					" << endl;
@@ -8,7 +15,7 @@ void east::printGame1() {
 	cout << endl;
 	cout << "					      BOS 107 - MIA 118                          " << endl;
 	cout << endl;
-	cout << "(F) Jayson Tatum: 29 PTS    6 AST    8 REB    10/21 FG (47.6%)" << endl;
+	cout << "(F) Jayson Tatum: " << JayTatum.printPTS(0) << " PTS    " << JayTatum.printAST(0) << " AST    " << JayTatum.printREB(0) << " REB    10 / 21 FG(47.6 %)" << endl;
 	cout << endl;
 	cout << "(F) Grant Williams: 7 PTS    2 AST    1 REB    2/5 FG (40%)" << endl;
 	cout << endl;
